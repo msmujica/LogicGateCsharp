332d002e6a6d102040b53df1a71430b050276290
Matias Hernandez <matiashernandezdonbosco@gmail.com>
2024-09-18 09:33:52 -0300

    OR

--- Gates/CompuertaOR.cs
+++ Gates/CompuertaOR.cs
@@ -1,6 +1,31 @@
 namespace Gates;
 
-public class CompuertaOR
+public class OrGate : ITruthValue 
 {
+    
+    private List<ITruthValue> inputs = new List<ITruthValue>();
+    private string Name { get; set; }
+        
+
+    public OrGate(string name, ITruthValue firstInput)
+    {
+        this.Name = name;
+        this.inputs.Add(firstInput);
+    }
+
+    public bool Evaluar
+    {
+        get
+        {
+            bool result = this.inputs[0].Evaluar;
+            for (int i = 1; i < this.inputs.Count; i++)
+            {
+                result = result || this.inputs[i].Evaluar;
+            }
+
+            return result;
+        }
+            
+    }
     
 }
